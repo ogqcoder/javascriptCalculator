28f96bcecdbdf41e14bba298d54940665e9aaceb
Garfield Phillips <61296776+ogqcoder@users.noreply.github.com>
2020-09-13 10:27:03 -0400

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var cont = document.getElementById("container");
+cont.onchange = function () {
+  var bill = Number(document.getElementById("billTotal").value);
+  var split = document.getElementById("splitInput").value;
+  var tip = document.getElementById("tipInput").value;
+  var tipValue = bill * (tip / 100);
+  var newBillEach = (bill + tipValue) / split;
+  var tipEach = tipValue / split;
+
+  document.getElementById("tipOutput").innerHTML = tip + "%";
+  document.getElementById("splitOutput").innerHTML = split;
+  document.getElementById("newBillId").innerHTML = newBillEach;
+  document.getElementById("tipEachOutput").innerHTML = tipEach;
+};
